964c4338927e5070a35d77322dcc6e4122123463
JakubKarnicki <jakub.karnicki@gmail.com>
2024-01-28 02:02:49 +0100

    Strona startowa, plus próba utworzenia bazy klientów

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Witaj w Banku");
+Console.WriteLine();
+Console.WriteLine("Wybierz z dostępnych opcji:");
+Console.WriteLine("1- Lista wszystkich klientów");
+Console.WriteLine("2- Logowanie");
+Console.WriteLine("3- Zakończ prace");
+Console.WriteLine("Aby wybrać naciśnij klawisz 1, 2 lub 3");
+Console.ReadKey();
