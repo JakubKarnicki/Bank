51017f61aa91d75bf4af6649872ed717700c87f8
JakubKarnicki <149016978+JakubKarnicki@users.noreply.github.com>
2024-01-28 14:02:02 +0100

    Update Customers.cs
    dodanie konstruktora oraz klasy klient

--- Customers.cs
+++ Customers.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace Bank
 {
-    internal class Customers;
+    public class Customers;
     public int Id { get; set; };
+    public decimal Balance { get; set; };
     public string Name { get; set; };
+    public int AccountNumber { get; set; };
 
+    public Customers (int id, decimal balance, string name, int accountNumber)
     {
+        Id = id;
+        Balance = balance;
+        Name = name;
+        AccountNumber = accountNumber;
     }
 }
