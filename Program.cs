cd23dec96a5b02fa6604032e887fecaafd2dfbc0
JakubKarnicki <149016978+JakubKarnicki@users.noreply.github.com>
2024-01-28 14:14:04 +0100

    Update Program.cs
    utworzenie klasy displayWelcome

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+public class displayWelcome
+{
 Console.WriteLine("Witaj w Banku");
 Console.WriteLine();
 Console.WriteLine("Wybierz z dostępnych opcji:");
@@ -7,3 +9,4 @@ Console.WriteLine("2- Logowanie");
 Console.WriteLine("3- Zakończ prace");
 Console.WriteLine("Aby wybrać naciśnij klawisz 1, 2 lub 3");
 Console.ReadKey();
+}
